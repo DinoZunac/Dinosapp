9eaf5563e5396b7d2f00db0eef002d5c991787a0
Dino Žunac <zunac.d@gmail.com>
2023-03-08 15:53:34 +0100

    fix:greške u konzoli2

--- src/App.js
+++ src/App.js
@@ -1,72 +1,202 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Messages from "./Messages";
-import Input from "./Input"
+import Input from "./Input";
 
 function randomName() {
-  const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  const adjectives = [
+    "autumn",
+    "hidden",
+    "bitter",
+    "misty",
+    "silent",
+    "empty",
+    "dry",
+    "dark",
+    "summer",
+    "icy",
+    "delicate",
+    "quiet",
+    "white",
+    "cool",
+    "spring",
+    "winter",
+    "patient",
+    "twilight",
+    "dawn",
+    "crimson",
+    "wispy",
+    "weathered",
+    "blue",
+    "billowing",
+    "broken",
+    "cold",
+    "damp",
+    "falling",
+    "frosty",
+    "green",
+    "long",
+    "late",
+    "lingering",
+    "bold",
+    "little",
+    "morning",
+    "muddy",
+    "old",
+    "red",
+    "rough",
+    "still",
+    "small",
+    "sparkling",
+    "throbbing",
+    "shy",
+    "wandering",
+    "withered",
+    "wild",
+    "black",
+    "young",
+    "holy",
+    "solitary",
+    "fragrant",
+    "aged",
+    "snowy",
+    "proud",
+    "floral",
+    "restless",
+    "divine",
+    "polished",
+    "ancient",
+    "purple",
+    "lively",
+    "nameless",
+  ];
+  const nouns = [
+    "waterfall",
+    "river",
+    "breeze",
+    "moon",
+    "rain",
+    "wind",
+    "sea",
+    "morning",
+    "snow",
+    "lake",
+    "sunset",
+    "pine",
+    "shadow",
+    "leaf",
+    "dawn",
+    "glitter",
+    "forest",
+    "hill",
+    "cloud",
+    "meadow",
+    "sun",
+    "glade",
+    "bird",
+    "brook",
+    "butterfly",
+    "bush",
+    "dew",
+    "dust",
+    "field",
+    "fire",
+    "flower",
+    "firefly",
+    "feather",
+    "grass",
+    "haze",
+    "mountain",
+    "night",
+    "pond",
+    "darkness",
+    "snowflake",
+    "silence",
+    "sound",
+    "sky",
+    "shape",
+    "surf",
+    "thunder",
+    "violet",
+    "water",
+    "wildflower",
+    "wave",
+    "water",
+    "resonance",
+    "sun",
+    "wood",
+    "dream",
+    "cherry",
+    "tree",
+    "fog",
+    "frost",
+    "voice",
+    "paper",
+    "frog",
+    "smoke",
+    "star",
+  ];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
   return adjective + noun;
 }
 
 function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
 }
 
 class App extends Component {
   state = {
+    id: "",
     messages: [],
     member: {
       username: randomName(),
       color: randomColor(),
-    }
-  }
-  constructor() {
-    super();
+    },
+  };
+  drone = null;
+
+  componentDidMount() {
     this.drone = new window.Scaledrone("IWsSfNVjfLzBJuo7", {
-      data: this.state.member
+      data: this.state.member,
     });
-    this.drone.on('open', error => {
+    this.drone.on("open", (error) => {
       if (error) {
         return console.error(error);
       }
-      const member = {...this.state.member};
+      const member = { ...this.state.member };
       member.id = this.drone.clientId;
-      this.setState({member});
+      this.setState({ member });
     });
     const room = this.drone.subscribe("observable-SobaZaChat");
-    room.on('data', (data, member) => {
+    room.on("data", (data, member) => {
       const messages = this.state.messages;
-      messages.push({member, text: data});
-      this.setState({messages});
+      messages.push({ member, text: data });
+      this.setState({ messages });
     });
   }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-        <h1>Dino's App</h1>
-      </div>
+          <h1>Dino's App</h1>
+        </div>
 
         <Messages
           messages={this.state.messages}
           currentMember={this.state.member}
         />
-        <Input
-        onSendMessage={this.onSendMessage}
-      />
+        <Input onSendMessage={this.onSendMessage} />
       </div>
     );
   }
   onSendMessage = (message) => {
     this.drone.publish({
       room: "observable-SobaZaChat",
-      message
+      message,
     });
-  }
+  };
 }
-
 
 export default App;
--- src/Messages.js
+++ src/Messages.js
@@ -6,18 +6,18 @@ class Messages extends Component {
     const {messages} = this.props;
     return (
       <ul className="Messages-list">
-        {messages.map(m => this.renderMessage(m))}
+        {messages.map((m, i) => this.renderMessage(m, i))}
       </ul>
     );
   }
-  renderMessage(message) {
+  renderMessage(message, index) { 
     const {member, text} = message;
     const {currentMember} = this.props;
     const messageFromMe = member.id === currentMember.id;
     const className = messageFromMe ?
       "Messages-message currentMember" : "Messages-message";
     return (
-      <li className={className}>
+      <li className={className} key={index}>
         <span
           className="avatar"
           style={{backgroundColor: member.clientData.color}}
